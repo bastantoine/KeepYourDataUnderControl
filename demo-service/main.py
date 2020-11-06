449dd7ab1287787cc184b4dd6394ad3d478f8463
bastantoine <bastien.antoine69+github@gmail.com>
2020-11-06 17:03:04 +0100

    Update main file to add connection to the SQLITE DB

--- demo-service/main.py
+++ demo-service/main.py
@@ -1,6 +1,14 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_there():
