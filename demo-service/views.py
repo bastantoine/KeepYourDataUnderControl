56edd06a3e66bce08d8bc05890d0302e4aa31e9e
bastantoine <bastien.antoine69+github@gmail.com>
2020-11-07 10:22:18 +0100

    Added POST handling of /posts
    GET will return all posts and related comments
    POST will allow to create a new post
    https://github.com/bastantoine/KeepYourDataUnderControl/blob/main/SPECS/API.md#posts

--- demo-service/views.py
+++ demo-service/views.py
@@ -1,14 +1,29 @@
 from flask import (
     Blueprint,
     jsonify,
+    request,
 )
 
-from models import Post
+from models import (
+    db,
+    Post,
+)
 
 api = Blueprint('api', __name__, url_prefix="")
 
-@api.route('posts')
+@api.route('posts', methods=['GET', 'POST'])
 def posts():
-    return jsonify({
-        'posts': [post.to_dict(extended=True) for post in Post.query.all()]
-    })
+    if request.method == 'GET':
+        return jsonify({
+            'posts': [post.to_dict(extended=True) for post in Post.query.all()]
+        })
+
+    if not request.json:
+        return jsonify()
+
+    new_post = Post(**request.json)
+    db.session.add(new_post)
+    db.session.commit()
+
+    db.session.refresh(new_post) # To have the id of the newly created object
+    return jsonify(new_post.to_dict())
