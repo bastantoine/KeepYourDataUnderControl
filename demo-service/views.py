bd96f8eb09f3025bf5ffbb355b6a475cf9e94369
bastantoine <bastien.antoine69+github@gmail.com>
2020-11-07 11:26:15 +0100

    Added PUT and DELETE handling of /posts/<int:id_post>

--- demo-service/views.py
+++ demo-service/views.py
@@ -27,3 +27,18 @@ def posts():
 
     db.session.refresh(new_post) # To have the id of the newly created object
     return jsonify(new_post.to_dict())
+
+@api.route('/posts/<int:id_post>', methods=['PUT', 'DELETE'])
+def posts_id(id_post):
+    post = Post.query.get_or_404(id_post)
+
+    if request.method == 'PUT':
+        post.link = request.json.get('link', post.link)
+        db.session.commit()
+
+        db.session.refresh(post)
+        return jsonify(post.to_dict())
+
+    db.session.delete(post)
+    db.session.commit()
+    return ('', 200)
