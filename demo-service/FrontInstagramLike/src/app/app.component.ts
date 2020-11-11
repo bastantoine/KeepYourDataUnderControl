22889616eca12a5793bc80d002e602c7abe688d0
bastantoine <bastien.antoine69+github@gmail.com>
2020-11-11 15:53:32 +0100

    Display all posts and their associated comments

--- demo-service/FrontInstagramLike/src/app/app.component.ts
+++ demo-service/FrontInstagramLike/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { PostService } from "./post.service";
+import { Post } from "./models";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FrontInstagramLike';
+export class AppComponent implements OnInit {
+
+  allPosts: Post[];
+
+  constructor(private post: PostService) { }
+
+  ngOnInit() {
+    this.allPosts = [];
+    this.getAllPosts();
+  }
+
+  getAllPosts() {
+    this.post.getAllPosts().subscribe(data => this.allPosts = data.posts);
+  }
+
 }
