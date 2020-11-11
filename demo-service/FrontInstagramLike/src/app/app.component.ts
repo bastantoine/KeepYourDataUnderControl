84a0a23ffee843defbf5b88b69cf2691d28475cd
bastantoine <bastien.antoine69+github@gmail.com>
2020-11-11 17:31:54 +0100

    Added form to add a comment to a post

--- demo-service/FrontInstagramLike/src/app/app.component.ts
+++ demo-service/FrontInstagramLike/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 import { PostService } from "./post.service";
 import { Post } from "./models";
@@ -11,8 +12,16 @@ import { Post } from "./models";
 export class AppComponent implements OnInit {
 
   allPosts: Post[];
+  form_add_comment: FormGroup;
 
-  constructor(private post: PostService) { }
+  constructor(
+    private post: PostService,
+    private form_builder: FormBuilder,
+  ) {
+    this.form_add_comment = this.form_builder.group({
+      link: '',
+    });
+  }
 
   ngOnInit() {
     this.allPosts = [];
@@ -21,6 +30,10 @@ export class AppComponent implements OnInit {
 
   getAllPosts() {
     this.post.getAllPosts().subscribe(data => this.allPosts = data.posts);
+  }
+
+  addComment(id_post: Number, data) {
+    console.log(id_post, data);
   }
 
 }
