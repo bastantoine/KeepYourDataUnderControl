43c9045241b539112a9dd325d3322fa318bbebe5
bastantoine <bastien.antoine69+github@gmail.com>
2020-11-11 17:30:35 +0100

    Added CommentService to make comment related API calls

---
+++ demo-service/FrontInstagramLike/src/app/comment.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+
+import { ApiService } from "./api.service";
+import { Comment } from "./models";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private api: ApiService) { }
+
+  createCommentOnPost(id_post: Number, link: String): Observable<Comment> {
+    return this.api.post<Comment>(["posts", String(id_post), "comments"], {"link": link});
+  }
+
+}
