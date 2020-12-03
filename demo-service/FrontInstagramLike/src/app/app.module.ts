8910b59ef07165a21d9a4baa21ccb4d3d1d8c355
bastantoine <bastien.antoine69+github@gmail.com>
2020-12-03 13:08:28 +0100

    Added EVidComponent

--- demo-service/FrontInstagramLike/src/app/app.module.ts
+++ demo-service/FrontInstagramLike/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { AppComponent } from './app.component';
 import { EImgComponent } from './html-elements/e-img/e-img.component';
 import { ETxtComponent } from './html-elements/e-txt/e-txt.component';
+import { EVidComponent } from './html-elements/e-vid/e-vid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EImgComponent,
     ETxtComponent,
+    EVidComponent,
   ],
   imports: [
     BrowserModule,
---
+++ demo-service/FrontInstagramLike/src/app/html-elements/e-vid/e-vid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-e-vid',
+  templateUrl: './e-vid.component.html',
+  styleUrls: ['./e-vid.component.css']
+})
+export class EVidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
