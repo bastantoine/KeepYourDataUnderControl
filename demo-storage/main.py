7a9762121bd9b5df3999eae3dbda1a30441c1a7f
bastantoine <bastien.antoine69+github@gmail.com>
2021-01-06 17:10:52 +0100

    Fix redirect on the debug page of the storage

--- demo-storage/main.py
+++ demo-storage/main.py
@@ -92,7 +92,7 @@ def debug_delete_all():
     for file in files_collection.find():
         files_collection.delete_one({"_id": file["_id"]})
         os.remove(os.path.join(app.config['UPLOAD_FOLDER'], file["link"]))
-    return redirect(url_for('debug_main'))
+    return redirect(os.path.join(os.environ.get("BASE_URL"), url_for('debug_main')))
 
 @app.route('/__debug__/delete/<key>')
 def debug_delete(key):
@@ -102,7 +102,7 @@ def debug_delete(key):
         abort(404)
     files_collection.delete_one({"_id": ObjectId(key)})
     os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return redirect(url_for('debug_main'))
+    return redirect(os.path.join(os.environ.get("BASE_URL"), url_for('debug_main')))
 
 if __name__ == '__main__':
    app.run()
