e633ed9a588a06e97bf3c9660d7a6ff23347b3b2
bastantoine <bastien.antoine69+github@gmail.com>
2021-02-08 16:32:22 +0100

    Send a file instead of a text to the api

--- demo-service/FrontInstagramLike/src/app/post.service.ts
+++ demo-service/FrontInstagramLike/src/app/post.service.ts
@@ -15,16 +15,16 @@ export class PostService {
     return this.api.get('posts')
   }
 
-  createPost(link: String): Observable<Post> {
-    return this.api.post<Post>("posts", {"link": link});
+  createPost(file: File): Observable<Post> {
+    return this.api.post<Post>("posts", this.api.generateFormDataForFile(file));
   }
 
   deletePost(id_post): Observable<Object> {
     return this.api.delete(["posts", String(id_post)]);
   }
 
-  editPost(id_post: Number, new_link: String): Observable<Post> {
-    return this.api.put(["posts", String(id_post)], {'link': new_link});
+  editPost(id_post: Number, new_file: File): Observable<Post> {
+    return this.api.put(["posts", String(id_post)], this.api.generateFormDataForFile(new_file));
   }
 
 }
