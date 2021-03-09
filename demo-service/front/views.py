17b55024c086de18d652dbaeaabe924c9a797160
bastantoine <bastien.antoine69+github@gmail.com>
2021-03-09 18:49:11 +0100

    Home view view template taken from the old version

--- demo-service/front/views.py
+++ demo-service/front/views.py
@@ -1,3 +1,20 @@
-from flask import Blueprint
+import os
+
+from dotenv import load_dotenv
+from flask import (
+    Blueprint,
+    render_template,
+)
+import requests
 
 views = Blueprint('views', __name__, url_prefix="")
+
+load_dotenv()
+API_ENDPOINT = os.environ.get('API_ENDPOINT')
+
+@views.route('/')
+def home():
+    req = requests.get(os.path.join(API_ENDPOINT, 'posts'))
+    if req.status_code == requests.codes.ok:
+        posts = req.json()['posts']
+        return render_template('home.html', posts=posts)
