443a734dc55610fc6f40f9a04e5b6323fc128b53
Lucas Dumestre <lucas.dumestre@wanadoo.fr>
2021-03-25 17:57:14 +0100

    Updated code of the extension to the latest version and a bit of cleanup

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, { file: "jquery-3.5.1.min.js" }, function() {
-        chrome.tabs.executeScript({file: 'content_script-2.js'});
+        chrome.tabs.executeScript(null, { file: "QrCode.js" }, function() {
+            chrome.tabs.executeScript({file: 'content_script-2.js'});
+        });
     });
 });
